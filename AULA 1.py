03f610f135013a25ce0b891e368dd8f63d695712
vicepassos98 <victorerycp@gmail.com>
2025-12-01 12:21:48 -0300

    AULA 1.py

---
+++ AULA 1.py
@@ -0,0 +1,6 @@
+#revisão de ontem: input e print. Ex. 2 - Guia de Cabreúva
+print("Olá meu nome é Labubu Osvaldo e serei seu guia por cabreúva, para isso preciso de algumas informações:")
+nome=input("Insira seu nome:")#-int serve para limitar as entradas a números inteiros
+idade=int(input("Insira sua idade:\n"))#\n serve para quebrar o texto para a linha de baixo
+cidade=input("Insira a cidade onde você mora:\n")
+print(f"Boa tarde {nome}, visto que você tem {idade} anos de idade e mora em {cidade} o melhor lugar para sair é o:\nLiceu Emaus")
